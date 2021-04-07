de34d0abafd5e7a0604d4cc2b54191360d46ee3f
Dawid <mateusiakdawid@gmail.com>
2021-04-07 19:33:58 +0200

    Add displaying texts with background on webcam output

--- videoanalyzer/videoanalyzer/video.py
+++ videoanalyzer/videoanalyzer/video.py
@@ -15,6 +15,18 @@ class VideoCamera:
         if colorMethod == "gray":
             self.colorMethod = cv2.COLOR_BGR2GRAY
 
+    @staticmethod
+    def get_text_size(text):
+        text_size, _ = cv2.getTextSize(text, cv2.FONT_HERSHEY_DUPLEX, 1, 2)
+        return text_size
+
+    @staticmethod
+    def display_text(image, text, position, text_width, text_height):
+        font = cv2.FONT_HERSHEY_DUPLEX
+        x, y = position
+        cv2.rectangle(image, (x, y), (x + text_width, y + text_height + 5), (255, 255, 255), -1)
+        cv2.putText(image, text, (x, y + text_height), font, 1, (0, 0, 0), 2, cv2.LINE_AA)
+
     def get_frame(self):
         grabbed, frame = self.video.read()
         width = int(self.video.get(3))*2
@@ -26,12 +38,26 @@ class VideoCamera:
             processed = cv2.cvtColor(frame, self.colorMethod)
             print(processed.shape)
         else:
+            #usunąć trzeci parametr z frame.shape?
             processed = frame
 
         output = np.zeros((height, width, frame.shape[2]), np.uint8)
         output[:height, :width // 2] = frame
         output[:height, width // 2:] = processed
+
+        # getting texts sizes
+
+        input_text_w, input_text_h = VideoCamera.get_text_size('INPUT')
+        output_text_w, output_text_h = VideoCamera.get_text_size('OUTPUT')
+
+        #displying texts backgrounds
+
+        VideoCamera.display_text(output, 'INPUT', (0,0), input_text_w, input_text_h)
+        VideoCamera.display_text(output, 'OUTPUT', (width//2, 0), output_text_w, output_text_h)
+
+        #extract image to .jpg format
         _, jpeg = cv2.imencode('.jpg', output)
+
         return jpeg.tobytes()
 
 
