829c803a2facf31c24339a55dfd3245e7721d6f7
Dawid <mateusiakdawid@gmail.com>
2021-04-12 20:46:49 +0200

    Create HTML templates for displaying functions

--- videoanalyzer/main/views.py
+++ videoanalyzer/main/views.py
@@ -8,41 +8,20 @@ from .models import DisplayModel
 from .forms import DisplayForm
 
 @gzip.gzip_page
-def clean_feed(request):
-    try:
-        cam = VideoCamera()
-        return StreamingHttpResponse(gen(cam), content_type="multipart/x-mixed-replace;boundary=frame")
-    except:  # This is bad! replace it with proper handling
-        pass
-
-@gzip.gzip_page
-def shape_feed(request):
+def circle_feed(request):
     try:
         cam = VideoCamera(shapeDetection="circle")
         return StreamingHttpResponse(gen(cam), content_type="multipart/x-mixed-replace;boundary=frame")
     except:  # This is bad! replace it with proper handling
         pass
-#
-# @gzip.gzip_page
-# def color_feed(request):
-#     try:
-#         cam = VideoCamera()
-#         return StreamingHttpResponse(gen(cam), content_type="multipart/x-mixed-replace;boundary=frame")
-#     except:  # This is bad! replace it with proper handling
-#         pass
-#
-# @gzip.gzip_page
-# def face_feed(request):
-#     try:
-#         cam = VideoCamera()
-#         return StreamingHttpResponse(gen(cam), content_type="multipart/x-mixed-replace;boundary=frame")
-#     except:  # This is bad! replace it with proper handling
-#         pass
+
 
 def home(request):
-    form = DisplayForm(request.POST or None, request.FILES or None)
-    mode = "clean"
-    if request.method == 'POST':
-        mode = request.POST.get('display_mode')
-    return render(request, "main/base.html", {"form": form, "mode": mode})
+    return render(request, "main/index.html")
+
+def shape(request):
+    return render(request, "main/shape.html")
+
+def detect_circle(request):
+    return render(request, "main/circle.html")
 
